dcf4fe9be0144d7f20a323214accdd6d26ae83cf
cecinuga <cecinuga.tdb@gmail.com>
2025-01-09 17:11:38 +0100

    climbing_stairs python

--- Easy/py/climbing_stairs.py
+++ Easy/py/climbing_stairs.py
@@ -1,3 +1,18 @@
+import math
+
 class Solution:
+    def binom(self, n: int, k: int) -> int:
+        return math.factorial(n)/(math.factorial(k)*math.factorial(n-k))
+    
     def climbStairs(self, n:int) -> int:
-        pass
+        tot = 0
+        for n_bit in range(math.floor(int(n)/2)+1):
+            tot += self.binom(n-n_bit, n_bit)
+        
+        return int(tot)
+
+#2 => 2
+#3 => 3
+#4 => 5
+solution = Solution()
+print(solution.climbStairs(6))
