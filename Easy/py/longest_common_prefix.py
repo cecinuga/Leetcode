42524ec2ae29a72ef1f0863523f6483a3ad35792
cecinuga <cecinuga.tdb@gmail.com>
2025-01-10 12:46:41 +0100

    -m

--- Easy/py/longest_common_prefix.py
+++ Easy/py/longest_common_prefix.py
@@ -1,3 +1,25 @@
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
-        pass
+        if len(min(strs)) == 0: return '' 
+
+        max_length_str = max(strs)        
+        strs.remove(max_length_str)
+        joined_strs = f"-{'-'.join(strs)}"
+
+        for i in range(len(max_length_str), 0, -1):
+            if joined_strs.count(f'-{max_length_str[:i]}') >= len(strs):
+                return max_length_str[:i]
+
+        return ''
+
+
+
+solution = Solution()
+
+test_case_neg = [ "dog","racecar","car" ]
+test_case_pos = ["aa","ab"]
+test_case_neg = ["c","acc","ccc"]
+test_case_pos = [ "flower","flow","flight" ]
+
+print(solution.longestCommonPrefix(test_case_pos))
+#print(solution.longestCommonPrefix(test_case_neg))
