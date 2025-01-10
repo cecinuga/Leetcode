0f047df3b5b62a939dff210fc80b772e723da4c9
cecinuga <cecinuga.tdb@gmail.com>
2025-01-10 12:54:56 +0100

    -m

--- git-pipeline.sh
+++ git-pipeline.sh
@@ -7,7 +7,7 @@ MARKPLATE=/home/cecinuga/Scrivania/Programming/markplate
 alias python=${MARKPLATE}'/env/lib/python3.12'
 source ${MARKPLATE}/env/bin/activate
 
-python ${MARKPLATE}/markplate.py --temp ${MARKPLATE}/templates/leetcode.jinja --out ./ --out-file README.md --cb ${MARKPLATE}/callbacks/recursive_file_visit.py --ex-cb-dir ./ --exclude [.git,.gitignore,README.md] -username cecinuga
+python ${MARKPLATE}/markplate.py --temp ${MARKPLATE}/templates/leetcode.jinja --out ./ --out-file README.md --cb ${MARKPLATE}/callbacks/recursive_file_visit.py --ex-cb-dir ./ --exclude [.git,.gitignore,README.md,git-pipeline.sh] -username cecinuga
 
 deactivate
 $GIT add .
