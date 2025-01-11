d62b792a073278794854e7e884097883f360dfda
cecinuga <cecinuga.tdb@gmail.com>
2025-01-11 01:07:21 +0100

    changed_callback

--- git-pipeline.sh
+++ git-pipeline.sh
@@ -4,12 +4,14 @@ args=("$@")
 GIT='git --git-dir='$PWD'/.git'
 MARKPLATE=/home/cecinuga/Scrivania/Programming/markplate
 
+echo $args
+
 alias python=${MARKPLATE}'/env/lib/python3.12'
+
 source ${MARKPLATE}/env/bin/activate
-
-python ${MARKPLATE}/markplate.py --temp ${MARKPLATE}/templates/leetcode.jinja --out ./ --out-file README.md --cb ${MARKPLATE}/callbacks/recursive_file_visit.py --ex-cb-dir ./ --exclude [.git,.gitignore,README.md,git-pipeline.sh] -username cecinuga
-
+python ${MARKPLATE}/markplate.py --temp ${MARKPLATE}/templates/leetcode.jinja --out ./ --out-file README.md --cb ${MARKPLATE}/callbacks/leetcode.py --ex-cb-dir ./ --exclude [.git,.gitignore,README.md,git-pipeline.sh] -username cecinuga
 deactivate
+
 $GIT add .
 $GIT commit -m ${args[0]}
 $GIT push
