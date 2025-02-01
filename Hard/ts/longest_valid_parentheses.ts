9f85ec1cc0200fe6a3ebdadc2350e0f95cfac484
cecinuga <cecinuga.tdb@gmail.com>
2025-02-01 01:43:45 +0100

    longest

--- Hard/ts/longest_valid_parentheses.ts
+++ Hard/ts/longest_valid_parentheses.ts
@@ -1,6 +1,45 @@
 function longestValidParentheses(s: string): number {
-    
-    
+    const stack: number[] = [-1];
+    let maxLen = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            stack.push(i);
+        } else { // s[i] === ')'
+            stack.pop();
+            if (stack.length === 0) {
+                stack.push(i);
+            } else {
+                maxLen = Math.max(maxLen, i - stack[stack.length - 1]);
+            }
+        }
+    }
+    return maxLen;
+}
 
-    return 0
-};
+
+console.log(longestValidParentheses("()(()"))
+function longestValidParenthesesSubstring(s: string): string {
+    const stack: number[] = [-1];
+    let maxLen = 0;
+    let startIndex = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            stack.push(i);
+        } else { // s[i] === ')'
+            stack.pop();
+            if (stack.length === 0) {
+                stack.push(i);
+            } else {
+                const currentLen = i - stack[stack.length - 1];
+                if (currentLen > maxLen) {
+                    maxLen = currentLen;
+                    startIndex = stack[stack.length - 1] + 1;
+                }
+            }
+        }
+    }
+    return s.substring(startIndex, startIndex + maxLen);
+}
+
+console.log(longestValidParenthesesSubstring("()(()"));
