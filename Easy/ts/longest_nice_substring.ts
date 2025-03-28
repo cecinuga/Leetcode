fb5d9b54c83206e583ad6991005f4c1c710c325f
cecinuga <cecinuga.tdb@gmail.com>
2025-03-29 00:20:41 +0100

    nice_buddy

---
+++ Easy/ts/longest_nice_substring.ts
@@ -0,0 +1,19 @@
+function longestNiceSubstring(s: string): string {
+    if (s.length < 2) return ""
+      const charSet = new Set(s.split(''))
+  
+    for (let i = 0; i < s.length; i++) {
+      const c = s[i]
+      if (charSet.has(c.toLowerCase()) && charSet.has(c.toUpperCase())) 
+        continue
+      
+      
+      const left  = longestNiceSubstring(s.substring(0, i))
+      const right = longestNiceSubstring(s.substring(i + 1))
+      
+      return left.length >= right.length ? left : right
+    }
+  
+    return s
+  }
+  
