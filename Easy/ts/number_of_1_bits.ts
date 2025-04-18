5867aefc85c69b2920b6abf916e4fe1b9655487a
cecinuga <cecinuga.tdb@gmail.com>
2025-04-18 16:36:00 +0200

    hW

--- Easy/ts/number_of_1_bits.ts
+++ Easy/ts/number_of_1_bits.ts
@@ -5,7 +5,7 @@ function hammingWeight(n: number): number {
     do {
         hw++
         exp = Math.floor(Math.log2(n))
-        n -= (2**(exp))
+        n -= 2**(exp)
     } while ( n > 0);
 
     return hw
