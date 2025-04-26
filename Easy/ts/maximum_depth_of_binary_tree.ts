25f26fa131d3cedba07111037ab44d2426795589
cecinuga <cecinuga.tdb@gmail.com>
2025-04-26 17:58:14 +0200

    max_depth

--- Easy/ts/maximum_depth_of_binary_tree.ts
+++ Easy/ts/maximum_depth_of_binary_tree.ts
@@ -9,28 +9,25 @@ class TreeNode {
     }
 }
 
-function minDepth(root: TreeNode | null): number {
+function maxDepth(root: TreeNode | null): number {
     if ( root == null ) return 0
     if ( root.left == null && root.right == null ) return 1
- 
-    const dsx = minDepth(root.left) + (root.left == null ? 0: 1)
-    const ddx = minDepth(root.right) + (root.right == null ? 0: 1)
-
-    if ( dsx == 0 ) return ddx
-    else if ( ddx == 0 ) return dsx
     
-    return Math.min(dsx, ddx);
+    const dsx = maxDepth(root.left) + 1
+    const ddx = maxDepth(root.right) + 1
+    
+    return Math.max(dsx, ddx)
 };
 
 
 [3,9,20,null,null,15,7]
 
+const dxdx = new TreeNode(7)
 const dxsx = new TreeNode(15)
-const sx = new TreeNode(9, null, dxsx)
-const dxdx = new TreeNode(7, null, sx)
-const dx = new TreeNode(20, null, dxdx)
-const root = new TreeNode(3, null, dx)
+const dx = new TreeNode(20, dxsx, dxdx)
+const sx = new TreeNode(9, null)
+const root = new TreeNode(3, sx, dx)
 
-const res = minDepth(root)
+const res = maxDepth(root)
 console.log(res)
 //[2,null,3,null,4,null,5,null,6]
