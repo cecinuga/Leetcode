13c786b59b38ea1dbd817d5f45b67ff67b3cda0e
cecinuga <cecinuga.tdb@gmail.com>
2025-06-17 22:14:36 +0200

    max_profit

--- best_time_to_buy_and_sell_stock_ii.ts
+++ best_time_to_buy_and_sell_stock_ii.ts
@@ -18,11 +18,3 @@ function maxProfit(prices: number[]): number {
 
     return maxProfit
 };
-
-const profit1 = maxProfit([7,1,5,3,6,4])
-const profit2 = maxProfit([1,2,3,4,5])
-const profit3 = maxProfit([34,82,16,74,55,46,44,25,96,80,51,62,73,26,76,30,20,30,55,6,3,93,74,80,8,13,3,82,1,35,68,22,81,63,77,41,51,84,36,46,86,71,5,65,65,91,97,57,92,96,57,97,74,33,19,42,44,22,12,26])
-
-console.log(profit1)
-console.log(profit2)
-console.log(profit3)
